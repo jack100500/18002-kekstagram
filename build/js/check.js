bacab69a008636e62464db5f5cc213cc2d3b4dd6
jack100500 <admin@localhost>
2016-03-29 22:51:15 -0700

    Причесал код немного

--- build/js/check.js
+++ build/js/check.js
@@ -5,15 +5,15 @@
  */
 function getMessage(a, b) {
     switch(typeof a) {
-        case ('boolean'): {
+        case ('boolean'):
             return (a
                 ? 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров'
                 : 'Переданное GIF-изображение не анимировано');
-        }
-        case ('number'): {
+            break;
+        case ('number'):
             return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
-        }
-        case ('object'): {
+            break;
+        case ('object'):
             var sum = 0, i = 0;
             
             if (Array.isArray(a) && Array.isArray(b)) {
@@ -28,9 +28,9 @@ function getMessage(a, b) {
             }
 
             return 'Количество красных точек во всех строчках изображения: ' + sum;
-        }
-        default: {
+            break;
+        default:
             return 'Неизвестный тип файла';
-        }
+            break;
     }
 }
